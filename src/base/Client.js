f1a2f31b28bd11b5cba4486501b589bfedcd351b
ZYROUGE <50770114+zyrouge@users.noreply.github.com>
2020-03-18 19:37:57 +0530

    Update Client.js

--- src/base/Client.js
+++ src/base/Client.js
@@ -15,16 +15,11 @@ client.commands = new Eris.Collection();
 client.aliases = new Eris.Collection();
 
 client.emojis = {
-    "tick": "<:yestick:649964798825463848>",
-    "settings": "<:settings:649965144746491914>",
-    "search": "<:search:649965040019177485>",
-    "loop": "<:repeaton:649964967889600522>",
-    "unloop": "<:repeatoff:649964893411213312>",
-    "remove": "<:remove:649965010247745558>",
-    "music": "<:musicicon:649964862386208769>",
-    "cross": "<:crosstick:649964831536971776>",
-    "add": "<:add:649965070389870603>",
-    "headphones": "<:headphonemusic:649965110395273226>"
+    "tick": "✅",
+    "search": "🔍",
+    "music": "🎵",
+    "cross": "❌",
+    "headphones": "🎧"
 };
 
 module.exports = client;
